d1ea53b522038b7802cd6c0323c8aedd02ad1c52
Jr Prévost <jr@ailleurs.me>
2014-05-17 00:05:23 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -21,7 +21,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         }
     },
     
-    handleKeyPress: {
+    handleKeypress: {
         value: function(evt) {
             // do nothing for now
             console.log(evt)
